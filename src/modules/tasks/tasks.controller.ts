57926b211806f4d332ab67f75ea73cd02135f764
abhishekkumar <suraj.kumar271@gmail.com>
2025-03-31 15:13:51 +0530

    added curd functionality

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,9 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe';
+import { CreateTaskDto, createTaskSchema } from './create-task.schema';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
   @Get()
-  sendHello() {
-    return 'Hello from hot reload';
+  async getTasks() {
+    return this.tasksService.getTasks();
+  }
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createTaskSchema))
+  async createTask(@Body() createTaskDto: CreateTaskDto) {
+    this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch()
+  async updateTask(@Body() obj: { id: string; data: any }) {
+    this.tasksService.updateTask(obj.id, obj.data);
+  }
+
+  @Get(':id')
+  async getTaskById(@Param('id') taskId: string) {
+    return this.tasksService.getTaskById(taskId);
+  }
+
+  @Delete(':id')
+  async deleteTask(@Param('id') taskId: string) {
+    return this.tasksService.deleteTask(taskId);
   }
 }
